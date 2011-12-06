eec65489ad21a4e6173684d3ad39cee04b87e1ba
Mathieu Lecarme <mathieu@garambrogne.net>
2011-12-06 22:19:32 +0100

    nodeunit => vows

--- test/test-ngrams.js
+++ test/test-ngrams.js
@@ -1,40 +1,41 @@
-var sys = require('sys'),
-colors = require('colors'),
-ngram = require('../lib/ngram');
+ var vows = require('vows'),
+    assert = require('assert'),
+    ngram = require('../lib/ngram');
 
+vows.describe('Ngram').addBatch({
 
-exports.testTokens = function(test) {
-	test.expect(1);
-	var sentence = "Je mange des petits pois pour la première fois";
-	//sys.debug(sentence.tokens());
-	test.equals(9, sentence.tokens().length, "tokenization");
-	test.done();
-};
+    'A sentence': {
+        topic: "Je mange des petits pois pour la première fois",
+        'should be tokenized': function(sentence) {
+            assert.equal(9, sentence.tokens().length);
+        },
+        'should be ngramed': function(sentence) {
+            var n = new ngram.Ngrams();
+            n.min = 3;
+            n.feedAll(sentence);
+            var ranks = n.ranks();
+            assert.equal(63, n.keys.length);
+        }
+    }
 
-exports.testNgrams = function(test) {
-	test.expect(1);
-	var n = new ngram.Ngrams();
-	n.min = 3;
-	n.feedAll('Je mange des carottes et des patates'.tokens());
-	var ranks = n.ranks();
-	test.equals(44, n.keys.length);
-	//sys.debug(sys.inspect(ranks));
-	test.done();
-};
+}).run();
+
+/*
 
 exports.testLm = function(test) {
-	test.expect(2);
-	var fp = new ngram.FingerPrint();
-	fp.register('fr', '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/french.lm');
-	fp.register('en', '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/english.lm');
-	fp.register('it', '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/italian.lm');
-	var n = new ngram.Ngrams();
-	n.min = 3;
-	n.feedAll('redis ça si tu es un homme'.tokens());
-	test.equals('fr', fp.guess(n));
-	n = new ngram.Ngrams();
-	n.min = 3;
-	n.feedAll('redis is a network tools'.tokens());
-	test.equals('en', fp.guess(n));
-	test.done();
+    test.expect(2);
+    var fp = new ngram.FingerPrint();
+    fp.register('fr', '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/french.lm');
+    fp.register('en', '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/english.lm');
+    fp.register('it', '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/italian.lm');
+    var n = new ngram.Ngrams();
+    n.min = 3;
+    n.feedAll('redis ça si tu es un homme'.tokens());
+    test.equals('fr', fp.guess(n));
+    n = new ngram.Ngrams();
+    n.min = 3;
+    n.feedAll('redis is a network tools'.tokens());
+    test.equals('en', fp.guess(n));
+    test.done();
 };
+*/
